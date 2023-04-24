5cc352bbbca8882d32f3d87258976b66d1fe3aea
Zuzana Černohousová <cernohz@natur.cuni.cz>
2023-04-24 19:18:26 +0200

    Add argparse support to parse_matches.py

--- usage/parse_matches.py
+++ usage/parse_matches.py
@@ -1,22 +1,25 @@
 from parsers.match_parsers import FTDNAMatchParser
-import sys
+import argparse
+
+args_parser = argparse.ArgumentParser()
+
+args_parser.add_argument("source")
+args_parser.add_argument("source_file")
+args_parser.add_argument("output_file")
+
+args = args_parser.parse_args()
 
 FTDNA_names = ["FamilyTreeDNA", "FTDNA", "ftdna", "ft"]
 GEDmatch_names = ["GEDmatch", "GEDMatch", "gedmatch", "ged", "gm"]
 
-source = sys.argv[1]
-
-if source in FTDNA_names:
+if args.source in FTDNA_names:
 	parser = FTDNAMatchParser()
 
-elif source in GEDmatch_names:
+elif args.source in GEDmatch_names:
 	raise Exception("Cannot parse GEDmatch data yet.")
 
 else:
 	raise Exception("unknown source")
 
-source_file = sys.argv[2]
-output_file = sys.argv[3]
-
-parser.parse_file(source_file)
-parser.save_to_file(output_file)
+parser.parse_file(args.source_file)
+parser.save_to_file(args.output_file)
