2197340d000a9fc1f9e3bd3d04d362c82102d285
Zuzana Černohousová <cernohz@natur.cuni.cz>
2023-04-25 13:35:57 +0200

    Add argparse support to parse_segments.py

--- usage/parse_segments.py
+++ usage/parse_segments.py
@@ -1,11 +1,17 @@
 from parsers.segment_data_parsers import FTDNASegmentParser
-import sys
-
+import argparse
 
 FTDNA_names = ["FamilyTreeDNA", "FTDNA", "ftdna", "ft"]
 GEDmatch_names = ["GEDmatch", "GEDMatch", "gedmatch", "ged", "gm"]
 
-source = sys.argv[1]
+args_parser = argparse.ArgumentParser()
+args_parser.add_argument("source_file")
+args_parser.add_argument("output_file")
+args_parser.add_argument("-s", "--source", required=True)
+
+args = args_parser.parse_args()
+
+source = args.source
 
 if source in FTDNA_names:
 	parser = FTDNASegmentParser()
@@ -16,8 +22,8 @@ elif source in GEDmatch_names:
 else:
 	raise Exception("unknown source")
 
-source_file = sys.argv[2]
-output_file = sys.argv[3]
+source_file = args.source_file
+output_file = args.output_file
 
 parser.parse_file(source_file)
 parser.save_to_file(output_file)
