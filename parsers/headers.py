f2113d509dd6cba11f35249558deb125cf07471f
Zuzana Černohousová <cernohz@natur.cuni.cz>
2023-05-24 13:16:24 +0200

    Divide script into hierarchical regions

--- parsers/headers.py
+++ parsers/headers.py
@@ -7,45 +7,7 @@ class Databases(Enum):
 	GEDMATCH = 1
 
 
-class MatchFormatEnum(Enum):
-	id = 0
-	name = 1
-	source = 2
-	total_cm = 3
-	largest_segment_cm = 4
-	mt_haplogroup = 5
-	y_haplogroup = 6
-	x_total_cm = 7
-	kit_age = 8
-	generations = 9
-	match_number = 10
-	kit_id = 11
-	e_mail = 12
-	ged_wiki_tree = 13
-	sex = 14
-	x_largest_segment_cm = 15
-	ged_match_source = 16
-	snps_overlap = 17
-	match_date = 18
-	relationship_range = 19
-	linked_relationship = 20
-	ancestral_surnames = 21
-	notes = 22
-	matching_bucket = 23
-
-
-class MatchFormat:
-	@property
-	def header(self):
-		return [
-			'ID', 'Name', 'Source', 'Total cM', 'Largest segment cM', 'mt haplogroup', 'Y haplogroup', 'X total cM',
-			'Kit age', 'Generations', 'Match number', 'Kit ID', 'E-mail', 'GED WikiTree', 'Sex',
-			'X largest segment cM', 'GEDmatch source', 'SNPs overlap', 'Match date', 'Relationship range',
-			'Linked relationship', 'Ancestral surnames', 'Notes', 'Matching bucket'
-		]
-
-	def get_index(self, column_name):
-		return self.header.index(column_name)
+# region Abstract classes
 
 
 class InputFormat(ABC):
@@ -80,46 +42,51 @@ class InputFormat(ABC):
 		return self.header[index]
 
 
-class FTDNAMatchFormat(InputFormat):
-	@property
-	def format_name(self):
-		return "FamilyTreeDNA"
+# endregion
 
-	@property
-	def __mapping(self):
-		return {
-			'Match Date': MatchFormatEnum.match_date,
-			'Relationship Range': MatchFormatEnum.relationship_range,
-			'Shared DNA': MatchFormatEnum.total_cm,
-			'Longest Block': MatchFormatEnum.largest_segment_cm,
-			'Linked Relationship': MatchFormatEnum.linked_relationship,
-			'Ancestral Surnames': MatchFormatEnum.ancestral_surnames,
-			'Y - DNA Haplogroup': MatchFormatEnum.y_haplogroup,
-			'mtDNA Haplogroup': MatchFormatEnum.mt_haplogroup,
-			'Notes': MatchFormatEnum.notes,
-			'Matching Bucket': MatchFormatEnum.matching_bucket,
-			'X - Match': MatchFormatEnum.x_total_cm
-		}
 
+# region Application formats
+# defined by this application
+
+class MatchFormat:
 	@property
 	def header(self):
 		return [
-			'Full Name', 'First Name', 'Middle Name', 'Last Name', 'Match Date', 'Relationship Range',
-			'Shared DNA', 'Longest Block', 'Linked Relationship', 'Ancestral Surnames', 'Y-DNA Haplogroup',
-			'mtDNA Haplogroup', 'Notes', 'Matching Bucket', 'X-Match'
+			'ID', 'Name', 'Source', 'Total cM', 'Largest segment cM', 'mt haplogroup', 'Y haplogroup', 'X total cM',
+			'Kit age', 'Generations', 'Match number', 'Kit ID', 'E-mail', 'GED WikiTree', 'Sex',
+			'X largest segment cM', 'GEDmatch source', 'SNPs overlap', 'Match date', 'Relationship range',
+			'Linked relationship', 'Ancestral surnames', 'Notes', 'Matching bucket'
 		]
 
+	def get_index(self, column_name):
+		return self.header.index(column_name)
 
-class SegmentFormatEnum:
-	segment_id = 0
-	id = 1
-	name = 2
-	chromosome_id = 3
-	start = 4
-	end = 5
-	length_cm = 6
-	snps = 7
-	density = 8
+
+class MatchFormatEnum(Enum):
+	id = 0
+	name = 1
+	source = 2
+	total_cm = 3
+	largest_segment_cm = 4
+	mt_haplogroup = 5
+	y_haplogroup = 6
+	x_total_cm = 7
+	kit_age = 8
+	generations = 9
+	match_number = 10
+	kit_id = 11
+	e_mail = 12
+	ged_wiki_tree = 13
+	sex = 14
+	x_largest_segment_cm = 15
+	ged_match_source = 16
+	snps_overlap = 17
+	match_date = 18
+	relationship_range = 19
+	linked_relationship = 20
+	ancestral_surnames = 21
+	notes = 22
+	matching_bucket = 23
 
 
 class SegmentFormat:
@@ -132,25 +99,16 @@ class SegmentFormat:
 		return self.header.index(column_name)
 
 
-class FTDNASegmentFormat(InputFormat):
-	@property
-	def format_name(self):
-		return "FamilyTreeDNA"
-
-	@property
-	def header(self):
-		return ['Match Name', 'Chromosome', 'Start Location', 'End Location', 'Centimorgans', 'Matching SNPs']
-
-	@property
-	def __mapping(self):
-		return {
-			'Match Name': SegmentFormatEnum.name,
-			'Chromosome': SegmentFormatEnum.chromosome_id,
-			'Start Location': SegmentFormatEnum.start,
-			'End Location': SegmentFormatEnum.end,
-			'Centimorgans': SegmentFormatEnum.length_cm,
-			'Matching SNPs': SegmentFormatEnum.snps
-		}
+class SegmentFormatEnum:
+	segment_id = 0
+	id = 1
+	name = 2
+	chromosome_id = 3
+	start = 4
+	end = 5
+	length_cm = 6
+	snps = 7
+	density = 8
 
 
 class SharedMatchesFormat:
@@ -184,3 +142,66 @@ class ClusterFormat:
 
 	def get_index(self, column_name):
 		return self.header.index(column_name)
+
+
+# endregion
+
+# region Source formats
+# region Match formats
+
+class FTDNAMatchFormat(InputFormat):
+	@property
+	def format_name(self):
+		return "FamilyTreeDNA"
+
+	@property
+	def __mapping(self):
+		return {
+			'Match Date': MatchFormatEnum.match_date,
+			'Relationship Range': MatchFormatEnum.relationship_range,
+			'Shared DNA': MatchFormatEnum.total_cm,
+			'Longest Block': MatchFormatEnum.largest_segment_cm,
+			'Linked Relationship': MatchFormatEnum.linked_relationship,
+			'Ancestral Surnames': MatchFormatEnum.ancestral_surnames,
+			'Y - DNA Haplogroup': MatchFormatEnum.y_haplogroup,
+			'mtDNA Haplogroup': MatchFormatEnum.mt_haplogroup,
+			'Notes': MatchFormatEnum.notes,
+			'Matching Bucket': MatchFormatEnum.matching_bucket,
+			'X - Match': MatchFormatEnum.x_total_cm
+		}
+
+	@property
+	def header(self):
+		return [
+			'Full Name', 'First Name', 'Middle Name', 'Last Name', 'Match Date', 'Relationship Range',
+			'Shared DNA', 'Longest Block', 'Linked Relationship', 'Ancestral Surnames', 'Y-DNA Haplogroup',
+			'mtDNA Haplogroup', 'Notes', 'Matching Bucket', 'X-Match'
+		]
+
+# endregion
+
+
+# region Segment formats
+
+class FTDNASegmentFormat(InputFormat):
+	@property
+	def format_name(self):
+		return "FamilyTreeDNA"
+
+	@property
+	def header(self):
+		return ['Match Name', 'Chromosome', 'Start Location', 'End Location', 'Centimorgans', 'Matching SNPs']
+
+	@property
+	def __mapping(self):
+		return {
+			'Match Name': SegmentFormatEnum.name,
+			'Chromosome': SegmentFormatEnum.chromosome_id,
+			'Start Location': SegmentFormatEnum.start,
+			'End Location': SegmentFormatEnum.end,
+			'Centimorgans': SegmentFormatEnum.length_cm,
+			'Matching SNPs': SegmentFormatEnum.snps
+		}
+
+# endregion
+# endregion
