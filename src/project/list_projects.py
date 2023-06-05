b3982a080f7d3dd2ebf7d1dfbfdead457b8ddcd4
Zuzana Černohousová <cernohz@natur.cuni.cz>
2023-06-05 17:58:55 +0200

    Create list projects feature

---
+++ src/project/list_projects.py
@@ -0,0 +1,21 @@
+import argparse
+import configparser
+import appdirs
+import os
+
+if __name__ == "__main__":
+	args_parser = argparse.ArgumentParser()
+	args_parser.add_argument("-p", "--paths", action="store_true")
+
+	args = args_parser.parse_args()
+
+	projects_config_path = os.path.join(appdirs.user_config_dir("genetic-genealogy"), "projects.ini")
+	cp = configparser.ConfigParser()
+	cp.read(projects_config_path)
+
+	if args.paths:
+		for name in cp["PROJECTS"]:
+			print("name= " + name + " ,path= " + cp["PROJECTS"][name])
+	else:
+		for name in cp["PROJECTS"]:
+			print("name= " + name)
