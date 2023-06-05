42ddac458edaf572f14659b6fd72fab69046f94a
Zuzana Černohousová <cernohz@natur.cuni.cz>
2023-06-05 17:04:30 +0200

    Read the project-specific settings file

--- src/config_reader.py
+++ src/config_reader.py
@@ -1,4 +1,6 @@
 import configparser
+import appdirs
+import os
 
 
 class ConfigReader:
@@ -6,14 +8,28 @@ class ConfigReader:
 
 	@staticmethod
 	def get_match_database_location():
-		config = configparser.ConfigParser()
-		config.read("settings.ini")
-		# todo somehow somewhere store the root of the project, than append the filenames to the path
-		return config['CSV_LOCATIONS']['match_database']
+		current_proj_path = ConfigReader.get_current_project_path()
+
+		project_config = configparser.ConfigParser()
+		project_config.read(os.path.join(current_proj_path, "settings.ini"))
+
+		return project_config['CSV_LOCATIONS']['match_database']
 
 	@staticmethod
 	def get_segment_database_location():
-		config = configparser.ConfigParser()
-		config.read("settings.ini")
+		current_proj_path = ConfigReader.get_current_project_path()
 
-		return config['CSV_LOCATIONS']['segment_database']
+		project_config = configparser.ConfigParser()
+		project_config.read(os.path.join(current_proj_path, "settings.ini"))
+
+		return project_config['CSV_LOCATIONS']['segment_database']
+
+	@staticmethod
+	def get_current_project_path() -> str:
+		projects_path = os.path.join(appdirs.user_config_dir("genetic-genealogy"), "projects.ini")
+
+		cp = configparser.ConfigParser()
+		cp.read(projects_path)
+
+		current_project = cp["CURRENT_PROJECT"]["current_project"]
+		return cp["PROJECTS"][current_project]
