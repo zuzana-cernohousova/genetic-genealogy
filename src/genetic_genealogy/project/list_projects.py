ef8e77753c2fd030b244b250b051a350c9a70da2
Zuzana Černohousová <cernohz@natur.cuni.cz>
2023-06-07 11:32:43 +0200

    Rename argument

--- src/genetic_genealogy/project/list_projects.py
+++ src/genetic_genealogy/project/list_projects.py
@@ -5,14 +5,13 @@ import os
 
 
 def list_projects(args):
-
 	projects_config_path = os.path.join(appdirs.user_config_dir("genetic-genealogy"), "projects.ini")
 	cp = configparser.ConfigParser()
 	cp.read(projects_config_path)
 
-	if args.paths:
+	if args.long:
 		for name in cp["PROJECTS"]:
-			print("name= " + name + " ,path= " + cp["PROJECTS"][name])
+			print("name= " + name + " path= " + cp["PROJECTS"][name])
 	else:
 		for name in cp["PROJECTS"]:
 			print("name= " + name)
@@ -20,7 +19,7 @@ def list_projects(args):
 
 if __name__ == "__main__":
 	args_parser = argparse.ArgumentParser()
-	args_parser.add_argument("-p", "--paths", action="store_true")
+	args_parser.add_argument("-l", "--long", action="store_true")
 
 	arguments = args_parser.parse_args()
 
